ccd73ad4c4474daad2cd10662de29a26af86818b
57866696+thewooniverse@users.noreply.github.com <Joo@wooyoungs-mbp.home>
2020-12-29 17:57:51 +0000

    completed password locker

---
+++ Reference/automate the boring stuff with Python/password_locker/pw.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+# pw.py - An insecure password locker program.
+
+import sys
+import pyperclip
+
+
+PASSWORDS = {'gmail': 'Ewo1238as0vkGBHERish',
+             'bills': 'paytoplay1239vjabj314',
+             }
+
+if len(sys.argv) < 2:
+    print('Usage: python pw.py [accuont] - copy account password')
+    sys.exit()
+
+account = sys.argv[1]  # first command line arg is the account name
+
+if account in PASSWORDS:  # same function as PASSWORDS.keys
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + ' copied to clipboard.')
+
+else:
+    print('There is no account named ' + account)
