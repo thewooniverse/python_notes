9d7f2df9f39d39d7984ece6d12639ca0336d0b09
TheWooniverse <57866696+thewooniverse@users.noreply.github.com>
2022-03-15 06:08:54 +0000

    code progressions

---
+++ 2022 woo python notes/automate_boring_stuff/cipher.py
@@ -0,0 +1,13 @@
+
+
+
+print(type('str'))
+# bytes_test = b'{ABC}'
+print(type(bytes_test))
+
+bytesval = "BYTESVAL HERE".encode()
+print(bytesval)
+print(type(bytesval))
+print(bytesval.decode())
+
+
--- 2022 woo python notes/automate_boring_stuff/password_keeper.py
+++ 2022 woo python notes/automate_boring_stuff/password_keeper.py
@@ -1,14 +1,34 @@
 #! python
 # password_keeper.py -
 # developed by thewooniverse
+"""
+References:
+https://stackoverflow.com/questions/2490334/simple-way-to-encode-a-string-according-to-a-password
+"""
+
+
+
 
 import sys, pyperclip, os
-
 # import encryption and decryption from cipher.py
+
+
 
 
 # load encrypted passwords from separate pw.csv file
 cwd = os.getcwd()
+
+# check if pw.csv is in respective directory
+# if not, create one
+
+
+# if yes, open
+
+
+
+
+
+
 
 
 ### user prompts ###
@@ -21,19 +41,27 @@ if len(sys.argv) < 2:
 
 
 
+# assign args passed by user to variables
 account = sys.argv[1]
 pin = sys.argv[2]
-try:
-    password = sys.argv.[3]
-except:
+if sys.argv[0] == set:
+    password = sys.argv[3]
+else:
     pass
 
 
 
-if account in PASSWORDS:
-    # in in py dicts will look for the keys anyway;
-    pyperclip.copy(PASSWORDS[account])
-    print(f"Password for {account} copied to clipboard")
 
-else:
-    print(f"There is no account called {account} in our database!")
+
+
+
+
+
+
+# if account in PASSWORDS:
+#     # in in py dicts will look for the keys anyway;
+#     pyperclip.copy(PASSWORDS[account])
+#     print(f"Password for {account} copied to clipboard")
+
+# else:
+#     print(f"There is no account called {account} in our database!")
