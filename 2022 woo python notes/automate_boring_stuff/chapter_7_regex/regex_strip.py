181f0a508caeed2d410b756bac3f9ae44b41bb99
woo <woo@tallyup.com>
2022-03-24 06:35:28 -0700

    started regex stripper

---
+++ 2022 woo python notes/automate_boring_stuff/chapter_7_regex/regex_strip.py
@@ -0,0 +1,11 @@
+#! python
+"""
+regex_strip.py - a regex_strip function that strips a passed text of a specific given string.
+
+"""
+
+import re
+
+
+def regex_strip(text, stripStr=' '):
+    pass
